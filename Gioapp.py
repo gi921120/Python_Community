61249a1e8bf0af09925bcada815a6a8a0b88055d
gi921120 <gio.garcia.d92@gmail.com>
2020-05-22 20:00:53 +1000

    New concepts and practice

--- Gioapp.py
+++ Gioapp.py
@@ -3,3 +3,34 @@ age=int()
 BirthYear=input('Birth Year: ')
 age = BornYear(int(BirthYear))
 print(age)
+
+class PointClient:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y 
+    def move(self):
+        print("move")
+    
+    def draw(self):
+        print("draw")
+
+
+point1=PointClient(20,10) # Object definition 
+point1.draw() # access the method in the object 
+print(point1.x)
+
+class Person():
+    def __init__(self,name):
+        self.name =name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+wayne=Person("Wayne")
+#print(wayne.name)
+wayne.talk()
+
+
+from eco import ship
+
+ship.calcShip()
